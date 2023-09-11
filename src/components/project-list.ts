b39a67a0cfee5d3cd10b544cb384e659ff1db1dd
Uroš Todorović <todorovic.urosh@gmail.com>
2023-09-11 20:01:45 +0200

    webpack coinfigured

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { autobind } from '../decorators/autobind.js';
-import { Project, ProjectStatus } from '../models/project.js';
-import { DragTarget } from '../models/drag-drop.js';
-import { projectState } from '../state/project-state.js';
-import { Component } from './base.js';
-import { ProjectItem } from './project-item.js';
+import { autobind } from '../decorators/autobind';
+import { Project, ProjectStatus } from '../models/project';
+import { DragTarget } from '../models/drag-drop';
+import { projectState } from '../state/project-state';
+import { Component } from './base';
+import { ProjectItem } from './project-item';
 
 // ProjectList Class
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
